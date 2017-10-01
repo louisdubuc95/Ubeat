12918a63786698646b2fea970b3126f2c0261316
Rémi Weislinger <remi.weislinger.1@ulaval.ca>
2017-10-01 14:48:42 -0400

    Added the Apple Music link, fixed a wrong selector

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -5,7 +5,7 @@
         </div>
         <section id="artist-infos">
             <div class="container">
-                <h1>The Black Eyed Peas</h1>
+                <h1>The Black Eyed Peas <a href="https://geo.itunes.apple.com/ca/artist/the-black-eyed-peas/id360391?mt=1&app=music" style="display:inline-block;float:right;overflow:hidden;background:url(//linkmaker.itunes.apple.com/assets/shared/badges/en-us/music-lrg.svg) no-repeat;width:110px;height:40px;background-size:contain;"></a></h1>
                 <div id="artist-specs">
                     <p id="specs-genre">Hip-Hop / Rap</p>
                     <p id="specs-description">The Black Eyed Peas transcended hip-hop to become one of the most commercially successful pop groups of the 2000s. Their career began modestly, with a pair of albums inspired by the positive-minded likes of A Tribe Called Quest and De La Soul. After adding Fergie, the group's pop instincts reached full flight.</p>
@@ -22,27 +22,27 @@
                 <div class="pure-g">
                     <div class="artist-album pure-u-lg-1-5 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1">
                         <img class="pure-img" src="/static/images/artists/black-eyed-peas/albums/behind-the-front.jpeg" />
-                        <h3>Behind the front</h3>
+                        <h4>Behind the front</h4>
                     </div>
                     <div class="artist-album pure-u-lg-1-5 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1">
                         <img class="pure-img" src="/static/images/artists/black-eyed-peas/albums/the-beginning.jpeg" />
-                        <h3>The beginning</h3>
+                        <h4>The beginning</h4>
                     </div>
                     <div class="artist-album pure-u-lg-1-5 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1">
                         <img class="pure-img" src="/static/images/artists/black-eyed-peas/albums/bridging-the-gap.jpeg" />
-                        <h3>Bridging the gap</h3>
+                        <h4>Bridging the gap</h4>
                     </div>
                     <div class="artist-album pure-u-lg-1-5 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1">
                         <img class="pure-img" src="/static/images/artists/black-eyed-peas/albums/elephunk.jpeg" />
-                        <h3>Elephunk</h3>
+                        <h4>Elephunk</h4>
                     </div>
                     <div class="artist-album pure-u-lg-1-5 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1">
                         <img class="pure-img" src="/static/images/artists/black-eyed-peas/albums/monkey-business.jpeg" />
-                        <h3>Monkey business</h3>
+                        <h4>Monkey business</h4>
                     </div>
                     <div class="artist-album pure-u-lg-1-5 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1">
                         <img class="pure-img" src="/static/images/artists/black-eyed-peas/albums/the-end.jpeg" />
-                        <h3>The end</h3>
+                        <h4>The end</h4>
                     </div>
                 </div>
             </div>
