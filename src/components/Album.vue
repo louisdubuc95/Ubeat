0b1c6e7487dfa41fdee8b999f644adb9341c517e
louis-philippedubuc <louis95@hotmail.ca>
2017-11-08 14:46:20 -0500

    Album update APIcall. ajout de chanson avec leur titre et la longeur + extrait

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -3,7 +3,7 @@
         <section id="album-infos">
             <div class="pure-g container">
                 <div id="album-cover" class="pure-u-sm-1-4 pure-u-1">
-                    <img class="pure-img" :src='albumimg' width="100%"/>
+                    <img class="pure-img" :src='albumimg'/>
                 </div>
                 <div id="album-text" class="pure-u-sm-3-4 pure-u-1">
                     <h1>{{albumname}}</h1>
@@ -30,13 +30,13 @@
                 <div class="album-track pure-g" v-for="item in infoAlbum">
                   <div class="pure-u-2-24"><a class="pure-button pure-button-add" v-on:click="addSongToPlayList(item.number)" title='Add song to playlist'><i class="fa fa-plus" ></i></a></div>
                   <audio
-                      :id="item.number"
-                      :src="item.link"></audio>
-                      <div class="pure-u-2-24" v-if="!item.isPlaying"><a class="pure-button pure-button-play" v-on:click="manageAudio(item.number)"><i class="fa fa-play" >                    </i></a></div>
-                      <div class="pure-u-2-24" v-if="item.isPlaying"><a class="pure-button pure-button-pause" v-on:click="manageAudio(item.number)"><i class="fa fa-pause" >                    </i></a></div>
-                      <div class="pure-u-2-24">  {{ item.number }}  </div>
-                      <div class="pure-u-15-24">  {{ item.title }} </div>
-                      <div class="pure-u-3-24">  {{ item.length }} </div>
+                    :id="item.number"
+                    :src="item.link"></audio>
+                  <div class="pure-u-2-24" v-if="!item.isPlaying"><a class="pure-button pure-button-play" v-on:click="manageAudio(item.number)"><i class="fa fa-play" >                    </i></a></div>
+                  <div class="pure-u-2-24" v-if="item.isPlaying"><a class="pure-button pure-button-pause" v-on:click="manageAudio(item.number)"><i class="fa fa-pause" >                    </i></a></div>
+                  <div class="pure-u-2-24">  {{ item.number }}  </div>
+                  <div class="pure-u-15-24">  {{ item.title }} </div>
+                  <div class="pure-u-3-24">  {{ item.length }} </div>
                 </div>
             </div>
         </section>
@@ -53,42 +53,38 @@ import Vue from 'vue';
 export default {
   data() {
     return {
-      infoAlbum: [
-        { number: '1', title: 'The Time (Dirty Bit)', length: '5:07', link: 'https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music/00/03/29/mzm.lofaqvzx.aac.ep.m4a', isPlaying: false },
-        { number: '2', title: 'Light Up The Night', length: '4:21', link: 'https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music/ab/da/02/mzm.ahfimrgt.aac.ep.m4a', isPlaying: false },
-        { number: '3', title: 'Love You Long Time', length: '3:45', link: '', isPlaying: false },
-        { number: '4', title: 'XOXOXO', length: '3:45', link: '', isPlaying: false },
-        { number: '5', title: 'Someday', length: '4:33', link: '', isPlaying: false },
-        { number: '6', title: 'Whenever', length: '3:16', link: '', isPlaying: false },
-        { number: '7', title: 'Fashion Beats', length: '5:20', link: '', isPlaying: false },
-        { number: '8', title: 'Don\'t Stop The Party', length: '6:07', link: '', isPlaying: false },
-        { number: '9', title: 'Do It Like This', length: '5:29', link: '', isPlaying: false },
-        { number: '10', title: 'The Best One Yet (The Boy)', length: '4:25', link: '', isPlaying: false },
-        { number: '11', title: 'Just Can\'t Get Enough', length: '3:39', link: '', isPlaying: false },
-        { number: '12', title: 'Play It Loud', length: '4:21', link: '', isPlaying: false },
-      ],
-
-      id: '403881301',
-      info: [],
+      id: '1223592280',
+      artistname: '',
+      albumname: '',
       releaseDate: '',
       tracks: 0,
       genre: '',
-      artistname: '',
-      albumname: '',
-      albumimg: '',
-      lienItune: ''
+      lienItune: '',
+      infoAlbum: [],
     };
   },
   created() {
     Vue.http.get(`${UBeatUnsecureAPI.url}/albums/${this.id}`).then(response => (response.json()))
       .then((json) => {
-        this.releaseDate = json.results.length;
+        this.releaseDate = json.results[0].releaseDate;
         this.tracks = json.results[0].trackCount;
         this.genre = json.results[0].primaryGenreName;
         this.artistname = json.results[0].artistName;
         this.albumname = json.results[0].collectionName;
         this.albumimg = json.results[0].artworkUrl100;
         this.lienItune = json.results[0].collectionViewUrl;
+      });
+    Vue.http.get(`${UBeatUnsecureAPI.url}/albums/${this.id}/Tracks`).then(response => (response.json()))
+      .then((json) => {
+        for (let x = 0; x < json.results.length; x += 1) {
+          this.infoAlbum.push({
+            number: json.results[x].trackNumber,
+            title: json.results[x].trackName,
+            length: json.results[x].trackTimeMillis,
+            link: json.results[x].previewUrl,
+            isPlaying: false
+          });
+        }
       });
   },
   methods: {
