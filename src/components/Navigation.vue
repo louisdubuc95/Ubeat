a68a10ce5f4c29211af02aa44a4f54098d4be981
Rémi Weislinger <remi.weislinger.1@ulaval.ca>
2017-11-12 13:50:07 -0500

    Navigation: search wasn't disappearing on route change, fixed.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,18 +1,18 @@
 <template>
     <header>
         <nav>
-            <div id="menu-bars" v-on:click="displayMenu">
+            <div id="menu-bars" @click="toggleMenu">
                 <span><i class="fa fa-lg fa-bars"></i></span>
             </div>
             <div id="menu-left">
-                <router-link to="/" id="menu-home"><i class="fa fa-home"></i> <span>Home</span></router-link>
-                <router-link to="/playlists" class="menu-link"><i class="fa fa-folder-open"></i> Browse playlists</router-link>
+                <router-link to="/" @click.native="hideMenu" id="menu-home"><i class="fa fa-home"></i> <span>Home</span></router-link>
+                <router-link to="/playlists" @click.native="hideMenu" class="menu-link"><i class="fa fa-folder-open"></i> Browse playlists</router-link>
             </div>
             <div id="menu-right">
-                <input id="menu-search" type="search" placeholder="Looking for something ?" />
-                <router-link to="/" class="menu-link"><i class="fa fa-user"></i> <span>My profile</span></router-link>
-                <router-link to="/" class="menu-link"><i class="fa fa-cog"></i> <span>Settings</span></router-link>
-                <router-link to="/" class="menu-link"><i class="fa fa-sign-out"></i> <span>Sign out</span></router-link>
+                <input id="menu-search" class="menu-link" type="search" placeholder="Looking for something ?" />
+                <router-link to="/" @click.native="hideMenu" class="menu-link"><i class="fa fa-user"></i> <span>My profile</span></router-link>
+                <router-link to="/" @click.native="hideMenu" class="menu-link"><i class="fa fa-cog"></i> <span>Settings</span></router-link>
+                <router-link to="/" @click.native="hideMenu" class="menu-link"><i class="fa fa-sign-out"></i> <span>Sign out</span></router-link>
             </div>
         </nav>
     </header>
@@ -20,15 +20,28 @@
 
 <script>
 export default {
+  data() {
+    return {
+      items: null,
+      opened: false
+    };
+  },
+  created() {
+    this.items = document.getElementsByClassName('menu-link');
+  },
   methods: {
-    displayMenu: () => {
-      const menuLinks = document.getElementsByClassName('menu-link');
-      const menuSearch = document.getElementById('menu-search');
-
-      menuSearch.classList.toggle('menu-opened');
-
-      for (let i = 0; i < menuLinks.length; i += 1) {
-        menuLinks[i].classList.toggle('menu-opened');
+    hideMenu() {
+      if (this.opened) {
+        for (let i = 0; i < this.items.length; i += 1) {
+          this.items[i].classList.remove('menu-opened');
+        }
+        this.opened = false;
+      }
+    },
+    toggleMenu() {
+      this.opened = !this.opened;
+      for (let i = 0; i < this.items.length; i += 1) {
+        this.items[i].classList.toggle('menu-opened');
       }
     }
   }
