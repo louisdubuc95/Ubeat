bd6e728f77420881aa4ce4bfd03ee161a74104fc
Guillaume <guillaume.binet.1@ulaval.ca>
2017-12-16 13:53:39 -0500

    SearchApi, ajuster getToken.

--- src/assets/SearchApi.js
+++ src/assets/SearchApi.js
@@ -11,25 +11,25 @@ export default class SearchApi {
 
   static users(query, limit = 10) {
     return Vue.http.get(`search/users?q=${query}&limit=${limit}`,
-        { headers: { authorization: this.getToken() } })
+        { headers: { authorization: UsersApi.getToken() } })
       .then(response => response.json());
   }
 
   static albums(query, limit = 10) {
     return Vue.http.get(`search/albums?q=${query}&limit=${limit}`,
-        { headers: { authorization: this.getToken() } })
+        { headers: { authorization: UsersApi.getToken() } })
       .then(response => response.json());
   }
 
   static artists(query, limit = 10) {
     return Vue.http.get(`search/artists?q=${query}&limit=${limit}`,
-        { headers: { authorization: this.getToken() } })
+        { headers: { authorization: UsersApi.getToken() } })
       .then(response => response.json());
   }
 
   static tracks(query, limit = 10) {
     return Vue.http.get(`search/tracks?q=${query}&limit=${limit}`,
-        { headers: { authorization: this.getToken() } })
+        { headers: { authorization: UsersApi.getToken() } })
       .then(response => response.json());
   }
 }
